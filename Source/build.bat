72981de674c91c32f4003faf0a97eec6c4ba1c2b
e-dog <elecdog@gmail.com>
2014-05-16 12:46:34 +0300

    Set KSP path once.

--- Source/build.bat
+++ Source/build.bat
@@ -1,12 +1,13 @@
 @echo off
+set KSP=C:\games\KSPtest
 %WINDIR%\Microsoft.NET\Framework64\v3.5\csc /nologo /t:library^
- /out:"C:\games\KSPtest\GameData\ProceduralFairings\ProceduralFairings.dll"^
- /r:"C:\games\KSPtest\GameData\ProceduralFairings\KSPAPIExtensions.dll"^
- /r:"C:\games\KSPtest\KSP_Data\Managed\Assembly-CSharp.dll"^
- /r:"C:\games\KSPtest\KSP_Data\Managed\UnityEngine.dll"^
+ /out:"%KSP%\GameData\ProceduralFairings\ProceduralFairings.dll"^
+ /r:"%KSP%\GameData\ProceduralFairings\KSPAPIExtensions.dll"^
+ /r:"%KSP%\KSP_Data\Managed\Assembly-CSharp.dll"^
+ /r:"%KSP%\KSP_Data\Managed\UnityEngine.dll"^
  NodeNumberTweaker.cs Resizers.cs ^
  FairingBase.cs FairingDecoupler.cs FairingSide.cs PayloadScan.cs ProcAdapter.cs utils.cs
 if errorlevel 1 goto exit
-copy /y C:\games\KSPtest\GameData\ProceduralFairings\ProceduralFairings.dll "C:\Steam\SteamApps\common\Kerbal Space Program\GameData\ProceduralFairings\"
+rem copy /y %KSP%\GameData\ProceduralFairings\ProceduralFairings.dll "C:\Steam\SteamApps\common\Kerbal Space Program\GameData\ProceduralFairings\"
 echo ======================================
 :exit
