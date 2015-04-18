4f053396fadf88fcefad1c64c1bf3585922c527c
e-dog <elecdog@gmail.com>
2015-04-18 22:10:53 +0300

    Actual shielding check.

--- Source/FairingShielding.cs
+++ Source/FairingShielding.cs
@@ -67,12 +67,10 @@ public class KzFairingBaseShielding : PartModule, IAirstreamShield
   }
 
 
-  void getFairingParams()
+  AttachNode[] getFairingParams()
   {
     // check attached side parts and get params
     var attached=part.findAttachNodes("connect");
-    int numSideParts=attached.Length;
-
     ProceduralFairingSide sf=null;
 
     for (int i=0; i<attached.Length; ++i)
@@ -91,7 +89,7 @@ public class KzFairingBaseShielding : PartModule, IAirstreamShield
       boundCylY0=boundCylY1=boundCylRad=0;
       lookupCenter=Vector3.zero;
       lookupRad=0;
-      return;
+      return null;
     }
 
     // get shape polyline
@@ -139,6 +137,8 @@ public class KzFairingBaseShielding : PartModule, IAirstreamShield
 
     lookupCenter=new Vector3(0, (y0+y1)*0.5f, 0);
     lookupRad=new Vector2(mr, (y1-y0)*0.5f).magnitude;
+
+    return attached;
   }
 
 
@@ -146,13 +146,78 @@ public class KzFairingBaseShielding : PartModule, IAirstreamShield
   {
     disableShielding();
 
-    getFairingParams();
+    var attached=getFairingParams();
+    if (!sideFairing) return;
 
-    //== get all parts in range
+    // get all parts in range
+    var parts=new List<Part>();
+    var colliders=Physics.OverlapSphere(part.transform.TransformPoint(lookupCenter), lookupRad, 1);
+    for (int i=colliders.Length-1; i>=0; --i)
+    {
+      var p=colliders[i].gameObject.GetComponentUpwards<Part>();
+      if (p!=null) parts.AddUnique(p);
+    }
 
-    //== filter parts
+    //== check if the top is closed in inline/adapter case
 
-    //== add shielding
+    // filter parts
+    float sizeSqr=lookupRad*lookupRad*4;
+    float boundCylRadSq=boundCylRad*boundCylRad;
+
+    for (int pi=0; pi<parts.Count; ++pi)
+    {
+      var pt=parts[pi];
+
+      // check special cases
+      if (pt==part) { shieldedParts.Add(pt); continue; }
+
+      bool isSide=false;
+      for (int i=0; i<attached.Length; ++i) if (attached[i].attachedPart==pt) { isSide=true; break; }
+      if (isSide) continue;
+
+      // check if too big to fit
+      var bounds=pt.GetRendererBounds();
+      if (PartGeometryUtil.MergeBounds(bounds, pt.partTransform).size.sqrMagnitude>sizeSqr) continue;
+
+      // check if the centroid is within fairing bounds
+      var c=part.transform.InverseTransformPoint(PartGeometryUtil.FindBoundsCentroid(bounds, null));
+
+      float y=c.y;
+      if (y<boundCylY0 || y>boundCylY1) continue;
+
+      float xsq=new Vector2(c.x, c.z).sqrMagnitude;
+      if (xsq>boundCylRadSq) continue;
+
+      // accurate centroid check
+      float x=Mathf.Sqrt(xsq);
+      bool inside=false;
+
+      for (int i=1; i<shape.Length; ++i)
+      {
+        var p0=shape[i-1];
+        var p1=shape[i];
+        if (p0.y>p1.y) { var p=p0; p0=p1; p1=p; }
+
+        if (y<p0.y || y>p1.y) continue;
+
+        float dy=p1.y-p0.y, r;
+        if (dy<=1e-6f) r=(p0.x+p1.x)*0.5f;
+        else r=(p1.x-p0.x)*(y-p0.y)/dy+p0.x;
+
+        if (x>r) continue;
+
+        inside=true;
+        break;
+      }
+
+      if (!inside) continue;
+
+      shieldedParts.Add(pt);
+    }
+
+    // add shielding
+    for (int i=0; i<shieldedParts.Count; ++i)
+      shieldedParts[i].AddShield(this);
   }
 
 
@@ -177,8 +242,8 @@ public class KzFairingBaseShielding : PartModule, IAirstreamShield
   {
     if (v!=vessel)
     {
-      var dp=v.vesselTransform.position - part.transform.TransformPoint(lookupCenter);
-      if (dp.sqrMagnitude > lookupRad*lookupRad) return;
+      var dp=v.vesselTransform.position-part.transform.TransformPoint(lookupCenter);
+      if (dp.sqrMagnitude>lookupRad*lookupRad) return;
     }
     enableShielding();
   }
@@ -198,9 +263,16 @@ public class KzFairingBaseShielding : PartModule, IAirstreamShield
 
   void OnPartDestroyed(Part p)
   {
-    if (p==part) disableShielding();
-    //== check for side fairing parts
-    //== check for top parts in inline/adapter case
+    if (p==part) { disableShielding(); return; }
+
+    // check for side fairing parts
+    var attached=part.findAttachNodes("connect");
+    for (int i=0; i<attached.Length; ++i)
+      if (p==attached[i].attachedPart) { disableShielding(); return; }
+
+    // check for top parts in inline/adapter case
+    if (p.vessel==vessel && sideFairing && sideFairing.inlineHeight>0)
+      enableShielding();
   }
 }
 
